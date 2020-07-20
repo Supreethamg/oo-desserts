ad0a1d1b8d775ef6e1c564f29c6bba35b612baf6
Supreetha Ganapathi <supreethamg@gmail.com>
2020-07-20 15:53:01 -0700

    practiced static and class methods

--- desserts.py
+++ desserts.py
@@ -3,12 +3,57 @@
 
 class Cupcake:
     """A cupcake."""
+    cache ={}
+    def __init__(self,name,flavor,price):
+      self.name = name
+      self.flavor = flavor
+      self.price = price
+      self.qty = 0
+      self.cache[self.name] = self
+
+
+    def add_stock(self,amount):
+      self.qty =self.qty + amount
+
+
+    def sell(self,amount):
+      if self.qty > 0:
+       self.qty = self.qty - amount
+       if self.qty < 0:
+         self.qty = 0
+      else:
+        print('Sorry, these cupcakes are sold out')
+
+    @staticmethod
+    def scale_recipe(ingredients,amount):
+      ing = []
+      for ingredient in ingredients:
+        ing.append((ingredient[0], ingredient[1] *amount))
+      return ing
+
+    @classmethod
+    def get(cls,name):
+      if name in cls.cache:
+        return cls.cache.get(name)
+      else:
+        print("Sorry, that cupcake doesn't exist")
+        
 
 
     def __repr__(self):
         """Human-readable printout for debugging."""
 
         return f'<Cupcake name="{self.name}" qty={self.qty}>'
+
+
+
+
+class Brownie(Cupcake):
+
+  def __init__(self,name,price):
+    super().__init__(self,name,"chocolate",price)
+
+
 
 
 if __name__ == '__main__':
